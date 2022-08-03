c9535c948b5ba621af4ebc24ff5d214bc66eb78f
ManuelDev <manueldeveloper17@gmail.com>
2022-08-02 22:54:40 -0500

    Finalizando proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 // Importando imagenmin
 const imagenmin = require("gulp-imagemin");
+// Importando purgeCss y rename
+const purgecss = require("gulp-purgecss");
+const rename = require("gulp-rename");
 
 // Funcion que compila codigo de SASS
 function css(done) {
@@ -37,9 +40,34 @@ function dev() {
   // A diferencia de la anterior, esta tarea inicia pero no finalzia
 }
 
+// Function para preparar proyecto a produccion
+function cssbuild(done) {
+  // Identificar archivo a reducir size
+  src("build/css/app.css")
+    // .pipe() para esperar la ejecucion de la funcion anterior y renombramos nombre del archivo
+    .pipe(
+      rename({
+        // Quedaria: app.min.css
+        suffix: ".min",
+      })
+    )
+    .pipe(
+      purgecss({
+        // En este caso solo pasamos un archivo, pero si hubieran mas podriamos hacerlo
+        content: ["index.html"],
+      })
+    )
+    // Exportarlo o guardarlo en una ubicacion
+    .pipe(dest("build/css"));
+
+  done();
+}
+
 // Para ejecutar la tarea abrir cmd y correr: gulp (nombre tarea exportada) -> gulp css
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 // Ejecutar por defecto multiples tareas, para correr ejecutar: gulp
 exports.default = series(imagenes, css, dev);
+// Ejecutar: gulp build (Preparamos archivo para produccion)
+exports.build = series(cssbuild);
